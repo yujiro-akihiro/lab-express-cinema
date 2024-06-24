fbc9e687504d92020faa73151b0add88bf476adb
Yujiro Akihiro <yujiro.akihiro@gmail.com>
2024-06-24 05:14:09 +0200

    #3 test

--- app.js
+++ app.js
@@ -1,42 +1,25 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
 require('dotenv/config');
-
-// ℹ️ Connects to the database
 require('./db/index');
-
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
 const express = require('express');
-
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
 const hbs = require('hbs');
 const path = require('path');
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
 
-// default value for title local
 const projectName = 'lab-express-cinema';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
-
 app.locals.title = `${capitalized(projectName)} - Generated with Ironlauncher`;
 
 // Middleware to serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-const index = require('./routes/index');
-app.use('/', index);
+app.use('/', require('./routes/index'));
+app.use('/', require('./routes/movies'));
 
-// Movies routes
-const moviesRouter = require('./routes/movies');
-app.use('/', moviesRouter);
-
-// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+// Error handling
 require('./error-handling')(app);
 
 module.exports = app;
